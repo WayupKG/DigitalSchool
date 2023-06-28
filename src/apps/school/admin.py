563865d8dda707100847a8eee66e26401abbc876
WayupKG <adikgk232323@gmail.com>
2023-06-28 22:59:29 +0600

    feat: created Models and register admin

--- src/apps/school/admin.py
+++ src/apps/school/admin.py
@@ -1,1 +1,25 @@
 from django.contrib import admin
+
+from .models import School, ClassRoom, RelationshipSchoolClassRoom, RelationshipClassRoomStudent
+
+
+class RelationshipSchoolClassRoomInline(admin.TabularInline):
+    model = RelationshipSchoolClassRoom
+    extra = 1
+
+
+class RelationshipClassRoomStudentInline(admin.TabularInline):
+    model = RelationshipClassRoomStudent
+    extra = 1
+
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')
+    inlines = [RelationshipSchoolClassRoomInline]
+
+
+@admin.register(ClassRoom)
+class ClassRoomAdmin(admin.ModelAdmin):
+    list_display = ('title', 'teacher', 'created_at', 'updated_at')
+    inlines = [RelationshipClassRoomStudentInline]
